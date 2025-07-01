4188a9df4bdc54bdd3e47f9b9260cb79efa6bbee
Nikita Ivanov <nikita03.1998@mail.ru>
2025-07-01 10:45:48 +0300

    ym callback

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayStation\MainController;
 use App\Http\Controllers\Ym\MainController as YmMainController;
@@ -15,5 +16,25 @@ Route::group(['prefix' => 'ps'], function () {
         Route::post('update-price-items', [YmMainController::class, 'prepareToUpdatePriceItems']);
         Route::post('send-stock-items', [YmMainController::class, 'prepareSendStockItems']);
         Route::post('items-show', [YmMainController::class, 'prepareToItemsShow']);
+    });
+});
+
+Route::group(['prefix' => 'ym'], function () {
+    Route::any('callback', function (Request $request) {
+
+        // 5.45.207.0/25
+        //141.8.142.0/25
+        //5.255.253.0/25
+
+        \Log::debug("ym callback", [
+            'request' => $request->all(),
+            'headers' => $request->headers->all(),
+        ]);
+
+        return response()->json([
+            'name' => 'marketplace.1gros.ru',
+            'time' => now('UTC')->toIso8601String(),
+            'version' => '0.0.1'
+        ]);
     });
 });
