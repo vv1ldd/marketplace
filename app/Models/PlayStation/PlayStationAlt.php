4fd826fb85cee5d6db562f6752b520bc65e3e9b7
Nikita Ivanov <nikita03.1998@mail.ru>
2025-07-06 22:08:28 +0300

    обработка заказа

---
+++ app/Models/PlayStation/PlayStationAlt.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\PlayStation;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlayStationAlt extends Model
+{
+    protected $fillable = [
+        'sku',
+        'data',
+        'base_price',
+        'price_with_discount',
+        'name',
+        'updated_at',
+        'send_to_ym_at',
+        'category_id',
+    ];
+}
