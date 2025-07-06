9519be7e62207d718bd3200b50cf2e4ec59645e1
Nikita Ivanov <nikita03.1998@mail.ru>
2025-07-06 23:37:56 +0300

    обработка заказа

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
 use App\Http\Controllers\CodeController;
+use App\Http\Middleware\AllowIframeForRoute;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
 
-Route::get('redeem', [CodeController::class, 'getCodeView'])->name('redeem');
-Route::post('redeem', [CodeController::class, 'checkCode'])->name('redeem-send')->middleware('throttle:5,1');
+Route::group(['middleware' => [AllowIframeForRoute::class]], function () {
+    Route::get('redeem', [CodeController::class, 'getCodeView'])->name('redeem');
+    Route::post('redeem', [CodeController::class, 'checkCode'])->name('redeem-send')->middleware('throttle:5,1');
 
-Route::get('form', [CodeController::class, 'getViewForm'])->name('form');
-Route::post('form', [CodeController::class, 'sendForm'])->name('form-send')->middleware('throttle:5,1');
+    Route::get('form', [CodeController::class, 'getViewForm'])->name('form');
+    Route::post('form', [CodeController::class, 'sendForm'])->name('form-send')->middleware('throttle:5,1');
 
-Route::get('finish', [CodeController::class, 'getFinishView'])->name('finish');
+    Route::get('finish', [CodeController::class, 'getFinishView'])->name('finish');
+});
+
+
