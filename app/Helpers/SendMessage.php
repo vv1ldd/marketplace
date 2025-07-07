b5facf0f6e5158a73c2553c5afe4bba76e4519d6
Nikita Ivanov <nikita03.1998@mail.ru>
2025-07-07 15:38:55 +0300

    tg

--- app/Helpers/SendMessage.php
+++ app/Helpers/SendMessage.php
@@ -21,18 +21,19 @@ class SendMessage
         if ($status === 'new') {
             $items = $order->items()->get();
 
-            $client_info = (object)$order->client_info;
+            $client_info = $order->client_info;
 
             $message = "Новый заказ #{$order->id}\n";
             $message .= "Статус: {$order->status}\n";
             $message .= "-------\n";
 
-            $message .= "Клиент:" . data_get($client_info, 'lastName', 'нет данных') . " " . data_get($client_info, 'firstName', 'нет данных') . " " . data_get($client_info, 'middleName', 'нет данных') . "\n";
-            $message .= "Телефон: " . data_get($client_info, 'phone', 'нет данных') . "\n";
-            $message .= "Почта: " . data_get($client_info, 'email', 'нет данных') . "\n";
+            $message .= "Данные покупателя от Яндекса:\n";
+            $message .= "ФИО: " . data_get($client_info, 'lastName', '-') . " " . data_get($client_info, 'firstName', '-') . " " . data_get($client_info, 'middleName', '-') . "\n";
+            $message .= "Телефон: " . data_get($client_info, 'phone', '-') . "\n";
+            $message .= "Почта: " . data_get($client_info, 'email', '-') . "\n";
             $message .= "-------\n";
-            $message .= "Товары:\n";
 
+            $message .= "Товары:\n";
             foreach ($items as $item) {
                 $message .= "{$item['sku']} - {$item['count']} шт. \n";
             }
@@ -41,6 +42,28 @@ class SendMessage
             $message .= "Активация по заказу #{$order->id}\n";
             $message .= "Активировано sku: {$order_item->sku}\n";
             $message .= "-------\n";
+            $message .= "Данные заполненной формы:\n";
+
+            $client_info = $order_item->client_info;
+
+            $message .= "ФИО: " . data_get($client_info, 'last_name', '-') . " " . data_get($client_info, 'first_name', '-') . "\n";
+            $message .= "Телефон: " . data_get($client_info, 'phone', '-') . "\n";
+            $message .= "Почта: " . data_get($client_info, 'email', '-') . "\n";
+            $message .= "-------\n";
+
+            $option = data_get($client_info, 'option');
+
+            if (!empty($option)) {
+                $message .= "Опция:\n";
+                $option = $option[0];
+
+                foreach ($option as $key => $value) {
+                    $message .= "{$key}: {$value}\n";
+                }
+            } else {
+                $message .= "Опция не выбрана\n";
+            }
+
 
         }
 
