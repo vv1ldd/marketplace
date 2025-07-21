f391712055d26fa01522e4579f3ced69ac289c9f
dllpl <nick.iv.dev@gmail.com>
2025-07-21 19:40:21 +0300

    front build

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -58,6 +58,7 @@ class AdminPanelProvider extends PanelProvider
             ->maxContentWidth('full')
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->spa();
     }
 }
