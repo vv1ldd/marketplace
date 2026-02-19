7227d03062e9bcb6ccb81bfce4c090a8c57868bf
w1ld-resonation <no_nombre_otro@icloud.com>
2026-02-19 01:20:36 -0300

    Fix route name in finish.blade.php

--- resources/views/redeem/finish.blade.php
+++ resources/views/redeem/finish.blade.php
@@ -6,7 +6,7 @@
     <div
         class="w-full bg-zinc-800 border border-zinc-700 @if(!session('is_frame')) max-w-xl rounded-2xl @endif shadow-xl sm:p-8 p-4">
         <h2 class="text-2xl font-bold text-white mb-6 text-center">Мы успешно приняли ваш заказ</h2>
-        <a href="{{session('is_frame') ? route('redeem.step1') . '?is_frame=1' : route('redeem.step1')}}">
+        <a href="{{session('is_frame') ? route('redeem.code') . '?is_frame=1' : route('redeem.code')}}">
             <button
                 class="cursor-pointer w-full bg-blue-600 hover:bg-blue-700 transition-colors duration-200 text-white font-semibold py-2 px-4 rounded-xl shadow-md focus:ring-2 focus:ring-blue-600 focus:outline-none">
                 Активировать новый код
