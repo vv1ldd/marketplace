6e217040a11e5e76cad7016e25f6f127c0f0cd62
Selim Ataniiazov <no_nombre_otro@icloud.com>
2026-02-19 00:12:44 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Providers;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,17 +16,16 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
         Gate::guessPolicyNamesUsing(function (string $modelClass) {
             return str_replace('Models', 'Policies', $modelClass) . 'Policy';
         });
+
         LogViewer::auth(function ($request) {
             return $request->user() && $request->user()->hasRole('super_admin');
         });
