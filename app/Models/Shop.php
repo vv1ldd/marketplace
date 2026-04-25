600d23420078ddec88bd0243efef882c517ac385
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-25 15:58:37 -0300

    Fix: add missing BelongsTo import in Shop model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Order\Order;
 use Filament\Models\Contracts\HasName;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Shop extends Model implements HasName
