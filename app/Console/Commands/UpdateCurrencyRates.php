fe8c1e0c3ddf041f0cd162e63da730c3690bc499
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-26 20:35:34 -0300

    Improvement: made UpdateCurrencyRates command generic to support any currency via {CODE}USDT pair logic

--- app/Console/Commands/UpdateCurrencyRates.php
+++ app/Console/Commands/UpdateCurrencyRates.php
@@ -27,27 +27,32 @@ class UpdateCurrencyRates extends Command
         $currencies = Currency::where('is_auto_update', true)->get();
 
         foreach ($currencies as $currency) {
+            $code = $currency->code;
             $rate = 0;
             
-            if ($currency->code === 'USD') {
+            if ($code === 'RUB') {
+                $rate = 1.0;
+            } elseif ($code === 'USD') {
                 $rate = $usdtRub;
-            } elseif ($currency->code === 'TRY') {
+            } elseif ($code === 'TRY') {
                 $rate = $usdtTry > 0 ? ($usdtRub / $usdtTry) : 0;
-            } elseif ($currency->code === 'EUR') {
-                // Binance doesn't always have direct EURRUB or EURUSDT with RUB 
-                // We'll use a standard EURUSD cross rate from Binance if available or proxy
-                $eurUsdt = (float)$binance->tickerPrice('EURUSDT');
-                $rate = $eurUsdt * $usdtRub;
-            } elseif ($currency->code === 'GBP') {
-                $gbpUsdt = (float)$binance->tickerPrice('GBPUSDT');
-                $rate = $gbpUsdt * $usdtRub;
+            } else {
+                // Generic lookup for other currencies (EUR, GBP, AUD, etc.)
+                // Fetch {CODE}USDT and multiply by USDTRUB
+                $pairPrice = (float)$binance->tickerPrice($code . 'USDT');
+                if ($pairPrice > 0) {
+                    $rate = $pairPrice * $usdtRub;
+                } else {
+                    // Try reverse pair if needed? (USDT{CODE}) - rarely needed for fiat
+                    // Or try {CODE}BTC -> BTCRUB? (Too complex for now)
+                }
             }
 
             if ($rate > 0) {
                 $currency->update(['rate_to_rub' => $rate]);
-                $this->info("Updated {$currency->code}: " . round($rate, 4));
+                $this->info("Updated {$code}: " . round($rate, 4));
             } else {
-                $this->warn("Could not update rate for {$currency->code}");
+                $this->warn("Could not update rate for {$code}. Check if Binance has {$code}USDT pair.");
             }
         }
 
