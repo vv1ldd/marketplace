112fbc4a5740c66f6c8b1b6f2e6527a9fe5ac8c2
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-26 19:24:49 -0300

    Database: added US PlayStation region with provided UUID to seeder

--- database/migrations/2026_04_26_165500_seed_initial_providers.php
+++ database/migrations/2026_04_26_165500_seed_initial_providers.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use App\Models\Provider;
+use App\Models\PlayStation\PlayStationRegion;
 
 return new class extends Migration
 {
@@ -34,6 +35,22 @@ return new class extends Migration
                 'name' => 'PlayStation Store (US Bundles)',
                 'is_active' => true,
                 'settings' => ['tax' => 0], // Bundles use card prices which already have tax
+            ]
+        );
+
+        PlayStationRegion::updateOrCreate(
+            ['id' => '44d8bb20-653e-431e-8ad0-c0a365f68d2f'],
+            [
+                'name' => 'United States',
+                'slug' => 'US',
+            ]
+        );
+
+        PlayStationRegion::updateOrCreate(
+            ['id' => '063101db-9ac0-4e48-a948-29fe7e3f8dec'],
+            [
+                'name' => 'Turkey',
+                'slug' => 'TR',
             ]
         );
     }
