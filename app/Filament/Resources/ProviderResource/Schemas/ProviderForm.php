4bad5f39a7e14ac6c5119ab9bd8d34fea9e27ba8
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-26 18:57:43 -0300

    Fix: correct namespaces for form components in ProviderResource (v4 compatibility)

--- app/Filament/Resources/ProviderResource/Schemas/ProviderForm.php
+++ app/Filament/Resources/ProviderResource/Schemas/ProviderForm.php
@@ -2,8 +2,8 @@
 
 namespace App\Filament\Resources\ProviderResource\Schemas;
 
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
